4db740d552188d4c246b1a38e142789f1e425680
zablon <zablonoigo51@gmail.com>
2025-01-19 20:13:37 +0300

    register models

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User,Profile
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "email",
+        "is_email_verified",
+        "secret_key",
+        "otp_created_at",
+    ]
+    search_fields = ["username", "email"]
+    list_filter = ["otp_created_at"]
+    ordering = ["username"]
+    readonly_fields = ["secret_key", "otp_created_at"]
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=["user","date_joined","last_updated"]
+
+admin.site.register(Profile,ProfileAdmin)
+admin.site.register(User, UserAdmin)
