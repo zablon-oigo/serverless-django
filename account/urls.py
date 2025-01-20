8020a921e2fef22a2180b3e05bf2f2a957541f2c
zablon <zablonoigo51@gmail.com>
2025-01-20 08:03:51 +0300

    Add url path

---
+++ account/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import (
+    LoginUserView,
+    LogoutUserView,
+    PasswordResetConfirm,
+    PasswordResetRequestView,
+    RegisterUserView,
+    ResendOTPView,
+    SetNewPassword,
+    UserProfileListView,
+    UserProfileUpdateView,
+    VerifyOTPView
+)
+
+app_name = "account"
+urlpatterns = [
+    path("register/", RegisterUserView.as_view(), name="register"),
+    path("verify/", VerifyOTPView.as_view(), name="verify-otp"),
+    path("resend-otp/", ResendOTPView.as_view(), name="resend-otp"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("logout/", LogoutUserView.as_view(), name="logout"),
+    path("password-reset/", PasswordResetRequestView.as_view(), name="password-reset"),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        PasswordResetConfirm.as_view(),
+        name="password-reset-confirm",
+    ),
+    path("set-new-password/", SetNewPassword.as_view(), name="set-password"),
+    path("profile-update/", UserProfileUpdateView.as_view(), name="profile-update"),
+    path("profiles/", UserProfileListView.as_view(), name="user-profile-list"),
+]
