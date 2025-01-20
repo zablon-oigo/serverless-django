a9edc1a2080e75d67320c61a242c14c4a46a1ed5
zablon <zablonoigo51@gmail.com>
2025-01-20 08:04:43 +0300

    Add signal to trigger profile creation after email has been verified

---
+++ account/signals.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Profile
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def manage_user_profile(sender, instance, created, **kwargs):
+    if instance.is_email_verified:
+        Profile.objects.get_or_create(user=instance)
