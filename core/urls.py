8d844b64b43fa7a048070b18bdd6420e4215966a
zablon <zablonoigo51@gmail.com>
2025-01-21 07:39:55 +0300

    Add url path

--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/",include("account.urls"))
+    path("account/",include("account.urls")),
+    path("app/",include("app.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
+    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
