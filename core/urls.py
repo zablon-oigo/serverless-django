a0e5d2439fc661a9212733709d32a956a0d87697
zablon <zablonoigo51@gmail.com>
2025-01-25 10:27:58 +0300

    Add api doc path

--- core/urls.py
+++ core/urls.py
@@ -14,19 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,SpectacularSwaggerView
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/",include("account.urls")),
-    path("app/",include("app.urls")),
+    path("account/", include("account.urls")),
+    path("app/", include("app.urls")),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
-    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
