1a98c28d23106683b8f793dca8df36d2bb855fc3
zablon <zablonoigo51@gmail.com>
2025-02-03 21:59:09 +0300

    add account models

--- account/models.py
+++ account/models.py
@@ -1,9 +1,8 @@
-import os
 import uuid
-
+import os
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from PIL import Image
 
 
 class User(AbstractUser):
